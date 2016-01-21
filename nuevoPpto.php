937900c977b7f588c7458471cae60adda463a11f
Paco Ríos <pacoriosgalan@gmail.com>
2016-01-21 20:37:01 +0100

    Prueba para gitHub en archivo inservible editarPed.html. Última línea no válida

--- nuevoPpto.php
+++ nuevoPpto.php
@@ -8,14 +8,9 @@ include 'inc/conexion.php';
 // Obtener los datos que nos envía jquery 
 extract($_REQUEST); //A partir de esta línea tenemos disponibles unas variables que se llaman igual que el atributo name de los inputs del formulario. 
 
-$cliente_newPpto = isset($_REQUEST['cliente_newPpto']) ? $_REQUEST['cliente_newPpto'] : 1;
+
+echo ($_REQUEST['descripcion'][0] + $_REQUEST['check'][1] + $_REQUEST['precio'][2]+ $_REQUEST['pvp'][3] + $_REQUEST['dto'][4] + $_REQUEST['total'][5] + $_REQUEST['uds'][6]);
 
 
-echo $cliente_newPpto;
 
-/*if (no trae artículos){
-    echo -1;
-}
-else{
-    
-}*/
+

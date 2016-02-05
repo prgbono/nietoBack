97d9276adf52dae418da8c5e8e995bd8a36a6854
Paco Ríos <pacoriosgalan@gmail.com>
2016-02-05 10:43:58 +0100

    Pasar articulos de nuevo presupuesto en un vector (array)

--- nuevoPpto.php
+++ nuevoPpto.php
@@ -1,32 +1,28 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-
-//Incluir el fichero de conexión para conectar con la BBDD
 include 'inc/conexion.php';
 
-// Obtener los datos que nos envía jquery 
-extract($_REQUEST); //A partir de esta línea tenemos disponibles unas variables que se llaman igual que el atributo name de los inputs del formulario. 
+extract($_REQUEST); 
 
-//$fecha_newPpto = $_REQUEST['fecha_newPpto'];
-//$asunto_newPpto = $_REQUEST['asunto_newPpto'];
-//$id_coche = $_REQUEST['id_coche'];
-//$id_cliente = $_REQUEST['id_cliente'];
-//$transporte_newPpto = $_REQUEST['transporte_newPpto'];
-//$canarias_newPpto = $_REQUEST['canarias_newPpto'];
+$fecha_newPpto = isset($_REQUEST['fecha_newPpto']) ? $_REQUEST['fecha_newPpto'] : 'jopeta';
 
-$fecha_newPpto = '17-2-2016';
-$asunto_newPpto = asunto;
-$id_coche = 1;
-$id_cliente = 1;
-$transporte_newPpto = 500;
-$canarias_newPpto = 1;
+/*usar array_filter para eliminar eltos vacíos en arrays dinámicos*/
+$descripcion=array_filter($_REQUEST[descripcion]);
+$ref=array_filter($_REQUEST[ref]);
+$check=array_filter($_REQUEST[check]);
+$uds=array_filter($_REQUEST[uds]);
+$precio=array_filter($_REQUEST[precio]);
+$cambio=array_filter($_REQUEST[cambio]);
+$pvp=array_filter($_REQUEST[pvp]);
+$dto=array_filter($_REQUEST[dto]);
+$total=array_filter($_REQUEST[total]);
 
 
-$query= "INSERT INTO pruebas_presupuestos (fecha, asunto, id_coche, id_cliente, total, transporte, canarias, subtotal, iva) VALUES ('$fecha_newPpto', '$asunto_newPpto', (SELECT id_coche from pruebas_coches WHERE (id_cliente = '$id_cliente' AND ppal=1)), '$id_cliente', 0, '$transporte_newPpto', '$canarias_newPpto', 0, 0)";
 
-
-echo $query;
+$a = sizeof($descripcion);
+echo $a;
+//echo $query;
 
 
 

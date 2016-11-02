876115591cc91605ef932801d62658c3fe34a300
Paco Ríos <pacoriosgalan@gmail.com>
2016-11-02 08:13:34 +0100

    Update conexion.php

--- inc/conexion.php
+++ inc/conexion.php
@@ -2,18 +2,6 @@
 
 //Conexión con la BBDD
 
-/*$database = "nieto";
-$user = "root";
-$password = "root";
-$server = "localhost";*/
-//RASPI
-//$password = "pacorios1982";
-
-$database = "db634677685";
-$user = "dbo634677685";
-$password = "pacorios1982";
-$server = "db634677685.db.1and1.com";
-//$server = "localhost";
 
 
 //Conexión

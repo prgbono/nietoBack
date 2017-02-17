fc170cb0cc5105207ff1a66ae949c3b17981f245
Paco Ríos <pacoriosgalan@gmail.com>
2017-02-17 21:01:58 +0100

    Eliminar artículos de un presupuesto eliminado (tabla detalles) Mostrar el formato de fechas dd-mm-YYYY obteniéndolo de la BBDD

--- eliminarPpto.php
+++ eliminarPpto.php
@@ -2,6 +2,10 @@
 header('Access-Control-Allow-Origin: *');
 include 'inc/conexion.php';
 extract($_REQUEST);
+
+//Eliminar ppto y todos sus artículos
+$query = "DELETE FROM pruebas_detalle_presupuestos WHERE id_ppto = $id_ppto";
+mysqli_query($link, $query);
 $query = "DELETE FROM pruebas_presupuestos WHERE id_ppto=$id_ppto";
 echo mysqli_query($link, $query);
 

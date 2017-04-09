fd7bdf7e6c2c92fd163fbb110902fea19864db37
Paco Ríos <pacoriosgalan@gmail.com>
2017-04-09 20:56:43 +0200

    Subir documento Excel para actualizar la BBDD

---
+++ actualizarBBDD.php
@@ -0,0 +1,45 @@
+<?php
+//require_once ('../dompdf/dompdf_config.inc.php');
+header('Access-Control-Allow-Origin: *');
+//PARA PRODUCCIÓN
+include ('../nietoBack/inc/conexion.php');
+
+if($_POST)
+{
+    foreach ($_POST as $clave=>$valor)
+    {
+		echo "El valor de $clave es: $valor"."\n";
+        echo "<br>";
+	}
+}
+
+if(isset($_POST["CSVdoc"]))
+{
+	$file = $_FILES['CSVdoc']['tmp_name'];
+	$handle = fopen($file, "r");
+	$c = 0;
+	while(($filesop = fgetcsv($handle, 1000, ",")) !== false)
+	{
+		$part_number = $filesop[0];
+		$title = $filesop[1];
+		$sp_title = $filesop[2];
+		$gbp = $filesop[3];
+
+		//$sql = mysql_query("INSERT INTO csv (name, email) VALUES ('$name','$email')");
+		$sql= "DELETE FROM bbdd";
+		mysqli_query($link, $sql);
+		$sql= "INSERT INTO bbdd (part_number, title, sp_title, gbp) VALUES ('$partNumber', '$title', '$titulo', '$gbp')";
+		mysqli_query($link, $sql);
+	}
+
+	if($sql){
+		echo "OK";
+	}else{
+		echo "KO";
+	}
+}
+else{
+	echo "KO";
+}
+
+?>

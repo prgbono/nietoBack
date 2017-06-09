6ce8daa0f2b90b822813a02af3bb25f5e1781b23
Paco Ríos <pacoriosgalan@gmail.com>
2017-06-09 20:03:23 +0200

    Pedidos anulacion

---
+++ cargarArticulosAnulacion.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include 'inc/conexion.php';
+
+$sql = "SELECT * FROM pruebas_detalle_presupuestos ";
+
+
+if (!is_null($id_ppto)){
+	$sql = $sql . "WHERE id_ppto = ".$id_ppto;
+} 
+$result = mysqli_query($link, $sql);
+
+
+//JSON
+$output= array();
+while($fila = mysqli_fetch_assoc($result)){
+    //$output[]= $fila; Con esta instrucción bastaría pero los registros con tildes los pone a null
+    $output[]=array_map('utf8_encode', $fila);
+
+}
+
+echo '{"Articulos":'.json_encode($output).'}';

386ec97c60e283b969616c2b7021311c1b9898bd
Paco Ríos <pacoriosgalan@gmail.com>
2017-06-13 00:14:53 +0200

    Apartado pérdidas WIP

---
+++ eliminarPerdida.php
@@ -0,0 +1,6 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include 'inc/conexion.php';
+extract($_POST);
+$query = "DELETE FROM pruebas_perdidas WHERE id_perdida=$id_perdida";
+echo mysqli_query($link, $query);

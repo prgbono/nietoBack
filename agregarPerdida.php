d3bdf7dd14667cf2cb1c8f28eda8b65905daf3b5
Paco Ríos <pacoriosgalan@gmail.com>
2017-06-17 20:42:19 +0200

    Fin funcionalidad de pérdidas

--- agregarPerdida.php
+++ agregarPerdida.php
@@ -9,7 +9,24 @@ $concepto = utf8_decode(trim($concepto));
 $coste = utf8_decode(trim($coste));
 $fecha = utf8_decode(trim($fecha));
 
-$query = "INSERT INTO pruebas_perdidas (id_pedido, concepto, coste, fecha) VALUES ('$pedido', '$concepto', '$coste', STR_TO_DATE('$fecha', '%d/%m/%Y'))";
-//echo $query;
+
+//Distinguir entre inserción y actualización mediante el campo $pedido recibido
+$query = "SELECT * FROM pruebas_perdidas WHERE id_pedido='$pedido'";
 $result = mysqli_query($link, $query);
+if (mysqli_num_rows($result)>0){
+    //Ya existe
+    $query= "UPDATE pruebas_perdidas SET id_pedido='$pedido', concepto= '$concepto', coste= '$coste', fecha='$fecha' WHERE id_pedido='$pedido'";
+    $result=  mysqli_query($link, $query);
+    echo -1;
+}else{
+    //no existe. Insertar
+    $query = "INSERT INTO pruebas_perdidas (id_pedido, concepto, coste, fecha) VALUES ('$pedido', '$concepto', '$coste', STR_TO_DATE('$fecha', '%d/%m/%Y'))";
+	//echo $query;
+	$result = mysqli_query($link, $query);
+    echo 1;
+}
+
+
+
+
 
---
+++ cargarPerdida.php
@@ -0,0 +1,12 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include 'inc/conexion.php';
+$id_bbdd = isset($_POST['part_number']) ? $_POST['part_number'] : NULL;
+$sql = "SELECT * FROM pruebas_perdidas WHERE id_perdida= '$id_perdida'";
+$result = mysqli_query($link, $sql);
+
+$output= array();
+while($fila = mysqli_fetch_assoc($result)){
+    $output[]=array_map('utf8_encode', $fila);
+}
+echo '{"Perdida":'.json_encode($output).'}';
